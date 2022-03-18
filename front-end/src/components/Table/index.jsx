31f4db69ab019cb40dc28029ba549b8b5c9c277a
Tiago <carvalho110591@gmail.com>
2022-03-18 19:10:46 -0300

    Added table basic html to Table component

--- front-end/src/components/Table/index.jsx
+++ front-end/src/components/Table/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import * as Styled from './styles';
 
+const trs = ['Type','Date','Value','Cpf','Card','Time','Shop Owner','Shop Name'];
+
 export const Table = (props) => {
 
     return (
-        <></>
+        <Styled.TableContainer>
+            <table>
+                <thead>
+                    {trs.map((name) => {
+                        <tr>{name}</tr>
+                    })}
+                </thead>
+                <tbody>
+                    <tr></tr>
+                </tbody>
+            </table>
+        </Styled.TableContainer>
     )
 }
