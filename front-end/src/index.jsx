ce1ab0c9f04f78c3494452e12b298feb3a5f30d8
Tiago <carvalho110591@gmail.com>
2022-03-18 18:19:29 -0300

    Imported Home to index

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './templates/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
