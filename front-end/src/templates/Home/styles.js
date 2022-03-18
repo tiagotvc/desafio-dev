1f189298a384d8ea9d3e6215b349afe733be2584
Tiago <carvalho110591@gmail.com>
2022-03-18 18:17:13 -0300

    Created Home Wrapper style

---
+++ front-end/src/templates/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  align-items:center;
+  justify-content:center;
+  border:1px solid gray;
+  padding:30px;
+  display:flex;
+  color:black;
+`;
