71dd25ec73a918fd62b0882fa574e80924d46484
Tiago <carvalho110591@gmail.com>
2022-03-19 18:51:27 -0300

    Created controler for parse string in back

--- back-end/fileParseApi/controllers/fileParse-ctrl.js
+++ back-end/fileParseApi/controllers/fileParse-ctrl.js
@@ -1,4 +1,37 @@
-fileParse = () => {
+let CNAB = new Object();
+let CNAB_ARRAY = [];
+
+function parseStringAndSave(line){
+    CNAB['type'] = line.substring(0,1);
+    CNAB['date'] = line.substring(1,8);
+    CNAB['value'] = line.substring(9,18);
+    CNAB['cpf'] = line.substring(19,29);
+    CNAB['card'] = line.substring(30,41);
+    CNAB['shop_owner'] = line.substring(47,61);
+    CNAB['shop_name'] = line.substring(62,80);
+    console.log(CNAB);
+
+}
+
+fileParse = async (req, res) => {
+    const file_buffer = req.files.file.data;
+    const file_ready_to_read = file_buffer.toString();
+    let text = file_ready_to_read.split("\n");
+
+    text.forEach(function(x, index) {
+        parseStringAndSave(x)
+     });
+  
+    
+     
+    
+
+
+ 
+  
+    
+
+    
     
 }
 
