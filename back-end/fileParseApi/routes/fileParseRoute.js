d1d8fe7b05a5d6f1afb85cf14a1b63b41c4b9030
Tiago <carvalho110591@gmail.com>
2022-03-22 01:26:07 -0300

    Created new route

--- back-end/fileParseApi/routes/fileParseRoute.js
+++ back-end/fileParseApi/routes/fileParseRoute.js
@@ -3,5 +3,6 @@ const fileParserController = require('../controllers/fileParse-ctrl');
 const router = express.Router();
      
 router.post('/fileParse', fileParserController.fileParse);
+router.get('/loadParsedData', fileParserController.loadParsedData);
 
 module.exports = router
