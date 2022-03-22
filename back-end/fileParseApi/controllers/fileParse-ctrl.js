b3fe63495d2fcda0cdf3f1dccc22fb8dc251b0e6
Tiago <carvalho110591@gmail.com>
2022-03-22 01:18:41 -0300

    Clearing controller code and added new function to get route

--- back-end/fileParseApi/controllers/fileParse-ctrl.js
+++ back-end/fileParseApi/controllers/fileParse-ctrl.js
@@ -1,40 +1,35 @@
-let CNAB = new Object();
-let CNAB_ARRAY = [];
-
-function parseStringAndSave(line){
-    CNAB['type'] = line.substring(0,1);
-    CNAB['date'] = line.substring(1,8);
-    CNAB['value'] = line.substring(9,18);
-    CNAB['cpf'] = line.substring(19,29);
-    CNAB['card'] = line.substring(30,41);
-    CNAB['shop_owner'] = line.substring(47,61);
-    CNAB['shop_name'] = line.substring(62,80);
-    console.log(CNAB);
-
-}
+const db = require('../models/index');
+const dev_challenge_utils = require('../utils/dev-challenge-utils');
+const FileData = db.fileData;
+const { getValueByType } = require('../models/fileData/fileData-repository');
 
 fileParse = async (req, res) => {
     const file_buffer = req.files.file.data;
     const file_ready_to_read = file_buffer.toString();
     let text = file_ready_to_read.split("\n");
+   
+    try{
+        const fileDataArray = dev_challenge_utils.mapLine(text);
+        const response = await FileData.bulkCreate(fileDataArray);
+        res.json({response:response, err:false});
+    }
+    catch(err){
+        res.json({response:err, err:true});
+    }
+}
 
-    text.forEach(function(x, index) {
-        parseStringAndSave(x)
-     });
-  
-    
-     
-    
-
-
- 
-  
-    
-
-    
-    
+loadParsedData = async (req, res) => {
+    try {
+        let response = JSON.stringify( await getValueByType(), null, 2 );
+        response = JSON.parse(response);
+        res.json({response: response, err:false});
+    }
+    catch(err) {
+        res.json({response:err, err:true});
+    }  
 }
 
 module.exports = {
-    fileParse
+    fileParse,
+    loadParsedData
 }
