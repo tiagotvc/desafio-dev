18b55a4262c9d48ddf9c0505fd9f299d142d3b49
Tiago <carvalho110591@gmail.com>
2022-03-22 21:00:07 -0300

    Changed get route name

--- front-end/src/api/fileParse/index.js
+++ front-end/src/api/fileParse/index.js
@@ -1,13 +1,17 @@
 import Axios from 'axios';
 
 const instance = Axios.create({
-    baseURL: 'http://localhost:3003',
+    baseURL: 'http://localhost:3001',
 });
 
-export function filaDataParse(file) {
+export function fileDataParse(file) {
     const formData = new FormData();
     formData.append("file", file);
-
-    const data = instance.post('/api/fileParse', formData);
+    const data = instance.post('/api/fileParse', formData)
     return data;
+}
+
+export async function getTest() {
+    const getting = instance.get('/api/loadParsedData');
+    return getting;
 }
